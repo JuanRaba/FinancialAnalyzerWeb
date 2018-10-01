54de76344476c0221f7aaccdbd765e19bc52d345
juanraba <jrabanillog@gmail.com>
2018-10-01 11:59:14 -0300

    rails routes and index with get to Charts

--- app/controllers/clp_controller.rb
+++ app/controllers/clp_controller.rb
@@ -1,6 +1,21 @@
 class ClpController < ApplicationController
-  def googleChartSBIFApiDataTest
+  def index
   end
-  def googleChartSBIFApiDollarTest
+  def ufsCharts
+    @start = params[:startMonth][0..3]
+    @end = params[:endMonth][0..3]
+    # swap values if operator missed order 
+    @end, @start =  @start, @end if @start > @end
+  end
+  def dolarCharts
+    @startY = params[:startMonth][0..3]
+    @startM = params[:startMonth][5..6]
+    @endY = params[:endMonth][0..3]
+    @endM = params[:endMonth][5..6]
+    # swap values if operator missed order 
+    if @startY > @endY
+      @endY, @startY =  @startY, @endY
+      @endM, @startM =  @startM, @endM
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: "clp#googleChartSBIFApiDollarTest"
+  get 'clp/ufsCharts', to: 'clp#ufsCharts'
+  get 'clp/dolarCharts', to: 'clp#dolarCharts'
+  
+  root to: "clp#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
