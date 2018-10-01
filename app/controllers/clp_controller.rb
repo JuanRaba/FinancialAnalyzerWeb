b42312c673b0f0792e8f528547c7d3dcdf52cc40
juanraba <jrabanillog@gmail.com>
2018-10-01 12:28:23 -0300

    links to get data from api ready, problem in get formato=json

--- app/controllers/clp_controller.rb
+++ app/controllers/clp_controller.rb
@@ -2,12 +2,18 @@ class ClpController < ApplicationController
   def index
   end
   def ufsCharts
-    @start = params[:startMonth][0..3]
-    @end = params[:endMonth][0..3]
-    # swap values if operator missed order 
-    @end, @start =  @start, @end if @start > @end
+    get_params
+    @apikey = 'formato=json not working, fix it'
+    @jsonUrl = "https://api.sbif.cl/api-sbifv3/recursos_api/uf/periodo/#{@startY}/#{@startM}/#{@endY}/#{@endM}?apikey=#{@apikey}&formato=xml"
   end
   def dolarCharts
+    get_params
+    @apikey = 'formato=json not working, fix it'
+    @jsonUrl = "https://api.sbif.cl/api-sbifv3/recursos_api/dolar/periodo/#{@startY}/#{@startM}/dias_i/01/#{@endY}/#{@endM}/dias_f/30?apikey=#{@apikey}&formato=json"
+  end
+
+  private
+  def get_params
     @startY = params[:startMonth][0..3]
     @startM = params[:startMonth][5..6]
     @endY = params[:endMonth][0..3]
@@ -17,5 +23,7 @@ class ClpController < ApplicationController
       @endY, @startY =  @startY, @endY
       @endM, @startM =  @startM, @endM
     end
+    # warning we are exposing our apikey to the world
+    @apikey = "a2e560226b0acec90f8893ace3d460edb8a087a3"
   end
 end
